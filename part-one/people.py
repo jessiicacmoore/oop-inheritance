366ab681d8b74f4e6aa2a79c5372e5051c570a7d
Jessica Moore <jessica.cmoore1@gmail.com>
2019-05-16 13:45:27 -0400

    Created instance method of learn for Student class and instance method of teach for Teacher class

--- part-one/people.py
+++ part-one/people.py
@@ -1,6 +1,9 @@
 class Student:
     """A class that represents a student"""
+    def learn(self):
+        return "I get it!"
 
 class Instructor:
     """A class that represents an instructor"""
-    
+    def teach(self):
+        return "An object is an instance of a class"
